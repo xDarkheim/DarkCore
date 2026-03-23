0900668e0c115f8630bf6907272e3e37701419b6
Dmytro Hovenko <dmytro.hovenko@gmail.com>
2026-03-22 21:40:35 -0500

    style: format .php-cs-fixer-risky.php configuration file
    - Align assignment operators for `$rules` configuration
    - Add missing space after the logical not operator in the type check
    - Remove trailing newline from the end of the file

--- .php-cs-fixer-risky.php
+++ .php-cs-fixer-risky.php
@@ -6,21 +6,20 @@ use PhpCsFixer\Config;
 
 $config = require __DIR__ . '/.php-cs-fixer.php';
 
-if (!$config instanceof Config) {
+if (! $config instanceof Config) {
     throw new RuntimeException('Expected Config from .php-cs-fixer.php');
 }
 
-$rules = $config->getRules();
+$rules                           = $config->getRules();
 $rules['@PHP8x4Migration:risky'] = true;
-$rules['random_api_migration'] = true;
-$rules['no_alias_functions'] = true;
-$rules['modernize_strpos'] = true;
-$rules['declare_strict_types'] = true;
-$rules['return_assignment'] = true;
-$rules['strict_comparison'] = true;
-$rules['void_return'] = true;
+$rules['random_api_migration']   = true;
+$rules['no_alias_functions']     = true;
+$rules['modernize_strpos']       = true;
+$rules['declare_strict_types']   = true;
+$rules['return_assignment']      = true;
+$rules['strict_comparison']      = true;
+$rules['void_return']            = true;
 
 return $config
     ->setRiskyAllowed(true)
     ->setRules($rules);
-
